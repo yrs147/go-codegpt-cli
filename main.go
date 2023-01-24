fcd7fb03f2590d9d6a9bab88e60b66941a693521
yrs147 <yashraj14700728@gmail.com>
2023-01-24 23:24:49 +0530

    Added file handling functionality

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
-
+	"os"
+	"strings"
+	"log"
 	"github.com/PullRequestInc/go-gpt3"
 	"github.com/spf13/viper"
 )
@@ -17,7 +19,7 @@ func main() {
 	ctx := context.Background()
 	client := gpt3.NewClient(apiKey)
 
-	constant input File = "./input.txt"
+	const inputFile = "./input.txt"
 	fileBytes, err := os.ReadFile(inputFile)
 	if err != nil {
 		log.Fatal("failed to read file: %v", err)
@@ -27,6 +29,23 @@ func main() {
 	msg := msgPrefix + string(fileBytes) + msgSuffix
 
 	outputBuilder := strings.Builder{}
-	// client.CompletionStreamWithEngine(ctx, gpt)
+	err = client.CompletionStreamWithEngine(ctx, gpt3.TextDavinci003Engine, gpt3.CompletionRequest{
+		Prompt: []string{
+			msg,
+		},
+		MaxTokens:   gpt3.IntPtr(3000),
+		Temperature: gpt3.Float32Ptr(0),
+	}, func(resp *gpt3.CompletionResponse) {
+		outputBuilder.WriteString(resp.Choices[0].Text)
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	output := strings.TrimSpace(outputBuilder.String())
+	const outPutFile = "./output.txt"
+	err = os.WriteFile(outPutFile, []byte(output), os.ModePerm)
+	if err != nil {
+		log.Fatalf("Failed to Read file : %v ", err)
+	}
 
 }
